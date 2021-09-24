7e8ffee0d03bcca70d61f3c2179f1a5c8de3955d
B. Hamza <57045314+mrv8x@users.noreply.github.com>
2021-09-24 08:19:52 +0100

    Update starter.py

--- starter.py
+++ starter.py
@@ -2,6 +2,7 @@ from firebase_admin import credentials, firestore, db
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
+
 from loguru import logger
 
 import os
